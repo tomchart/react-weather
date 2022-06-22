b783d73eafbc758fbd325530839f795c49b71fa7
Tom Hart <tommy-hart@hotmail.com>
2022-06-22 18:24:57 +0100

    autofocus searchbox, trim inputs, add today & forecast buttons

--- src/components/Searchbox.jsx
+++ src/components/Searchbox.jsx
@@ -1,16 +1,24 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { WeatherContext } from "../context/WeatherContext.js";
 
 function Searchbox() {
   const {
     searchInput,
     setSearchInput,
+    setSearchType,
     weatherVisible,
     setWeatherVisible
   } = useContext(WeatherContext);
+  const searchInputEl = useRef(null);
 
   function handleInput(event) {
-    setSearchInput(event.target.value);
+    if (event.target.value) {
+      setSearchInput(event.target.value.replace(/\s+/g, ''));
+    } else if (event.target.form[0].value) {
+      setSearchInput(event.target.form[0].value.replace(/\s+/g, ''));
+    } else {
+      return;
+    }
   }
 
   function makeSearch(event) {
@@ -18,8 +26,14 @@ function Searchbox() {
     if (searchInput.trim().length === 0) {
       return
     }
-    setWeatherVisible(prevWeatherVisible => !weatherVisible);
+    if (!weatherVisible) {
+      setWeatherVisible(prevWeatherVisible => !weatherVisible);
+    }
   }
+
+  useEffect(() => {
+    searchInputEl.current.focus();
+  }, []);
 
   return (
     <>
@@ -31,9 +45,11 @@ function Searchbox() {
         <input 
           type="text" 
           defaultValue={searchInput}
+          ref={searchInputEl}
           onKeyDown={event => {
             if (event.key === 'Enter') {
               handleInput(event);
+              setSearchType('today');
             } else if (event.key === 'Escape') {
               return;   // TODO add cancel function
             }
@@ -41,6 +57,24 @@ function Searchbox() {
           placeholder="Enter postcode/town/city" 
           className="mt-4 input input-bordered w-full max-w-xs" 
         />
+        <button 
+          className="btn btn-primary ml-4 mt-3"
+          onClick={event => {
+            handleInput(event)
+            setSearchType('today')
+          }}
+        >
+          today
+        </button>
+        <button 
+          className="btn btn-primary ml-2 mt-3"
+          onClick={event => {
+            handleInput(event)
+            setSearchType('forecast')
+          }}
+        >
+          forecast
+        </button>
       </form>
     </>
   )
