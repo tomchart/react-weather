82513ab29ab67f24be1534778678dce915cbb0d7
Tom Hart <tommy-hart@hotmail.com>
2022-06-22 18:26:25 +0100

    add more states to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css'
 import { WeatherContext } from "./context/WeatherContext.js";
 import Searchbox from "./components/Searchbox.jsx";
@@ -6,14 +6,26 @@ import WeatherResults from "./components/WeatherResults.jsx";
 
 function App() {
   const [searchInput, setSearchInput] = useState(null);
+  const [searchType, setSearchType] = useState(null);
   const [weatherVisible, setWeatherVisible] = useState(false);
+  const [results, setResults] = useState(null);
+  const apiUri = 'http://127.0.0.1:8000/api';
+  const apiRoutes = {
+    'today': apiUri + '/weather/today/',
+    'forecast': apiUri + '/weather/forecast/',
+  };
 
   return (
     <WeatherContext.Provider value={{
       searchInput,
       setSearchInput,
+      searchType,
+      setSearchType,
       weatherVisible,
       setWeatherVisible,
+      apiRoutes,
+      results,
+      setResults,
     }}>
       <div>
         <Searchbox />
