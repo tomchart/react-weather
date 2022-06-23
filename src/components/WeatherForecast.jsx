5f95f4d1e950e3d8a31bf9e7f70d784b96d8522f
Tom Hart <tommy-hart@hotmail.com>
2022-06-23 16:57:40 +0100

    calculate futurehour times & temps in WeatherForecast, draw echart

--- src/components/WeatherForecast.jsx
+++ src/components/WeatherForecast.jsx
@@ -1,41 +1,102 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext, useMemo } from "react";
 import { WeatherContext } from "../context/WeatherContext.js";
+import ReactECharts from 'echarts-for-react'; 
 
 function WeatherForecast () {
   const { 
     results,
   } = useContext(WeatherContext);
 
-  const [hours, setHours] = useState(null);
-
-  function stripHours() {
-    var allHours = [];
+  function stripFutureHourObjects() {
+    var futureHourObjects = [];
     results.days.map(days => {
-      days.hours.map(hour => {
-        allHours.push(hour);
+      console.log(days);
+      // filter only hours in the future that are divisible by 3 (for fewer points on graph)
+      // datetimes are a string so doing some ropey substringing but we move
+      days.hours.filter(hour => hour.datetimeEpoch > results.currentConditions.datetimeEpoch && hour.datetime.substring(0, 2) % 3 === 0).map(hour => {
+        futureHourObjects.push(hour);
       })
     })
-    return allHours;
+    return futureHourObjects;
+  }
+
+  function stripFutureHourDatetimes() {
+    var hourDatetimes = [];
+    futureHours.map(hour => {
+      hourDatetimes.push(hour.datetime.substring(0, hour.datetime.length - 3));
+    })
+    return hourDatetimes;
+  }
+
+  function stripFutureHourTemps() {
+    var hourTemps = [];
+    futureHours.map(hour => {
+      hourTemps.push(hour.temp);
+    })
+    return hourTemps;
+  }
+
+  const futureHours = useMemo(stripFutureHourObjects, [results]);
+  const futureHourDatetimes = useMemo(stripFutureHourDatetimes, [futureHours]);
+  const futureHourTemps = useMemo(stripFutureHourTemps, [futureHours]);
+
+  const options = {
+    grid: { show: false },
+    color: ['#ffd500'],
+    xAxis: {
+      name: 'Time',
+      nameLocation: 'center',
+      nameTextStyle: {
+        padding: [12, 0, 0, 0]
+      },
+      type: 'category',
+      data: futureHourDatetimes,
+      boundaryGap: false,
+    },
+    yAxis: {
+      show: false,
+      type: 'value',
+      name: 'Temperature (C°)',
+      nameTextStyle: {
+        padding: [6, 0, 0, 0]
+      },
+      min: (value) => {
+        return Math.round(value.min) - 2;
+      },
+      interval: 1,
+      axisLine: {
+        show: false,
+      },
+    },
+    series: [
+      {
+        data: futureHourTemps,
+        symbol: 'circle',
+        symbolSize: 8,
+        type: 'line',
+        smooth: true,
+        areaStyle: {
+          color: '#ffab00',
+        },
+        label: {
+          show: true,
+          formatter: '{@score}',
+          textBorderWidth: 0,
+          color: '#FFFFFF',
+        },
+      },
+    ],
+    tooltip: {
+      trigger: 'axis',
+    },
   };
 
-  useEffect(() => {
-    setHours(stripHours());
-  }, []);
-
-  // bug here when rendering locations with no future hours - breaks formatting when re-rendering a location that does have them
   return (
-    <>
-      { hours && (
-        <div className="flex inline-row">
-          {[...hours].filter(hour => hour.datetime > results.currentConditions.datetime).map((hour, index) => 
-            <div key={index} className="mr-8 mt-6">
-              <div className="flex">{hour.temp}°C</div>
-              <div className="flex">{hour.datetime.substring(0, hour.datetime.length - 3)}</div>
-            </div>
-          )}
-        </div>
+    <div className="w-1/2">
+      { futureHourDatetimes && futureHourTemps && (
+        <ReactECharts option={options} />
       )}
-    </>
+    </div>
   )
 }
 
