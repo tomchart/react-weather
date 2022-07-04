586865bebb3646ef29200a35e8b600d5f25c4bf7
Tom Hart <tommy-hart@hotmail.com>
2022-07-04 17:08:35 +0100

    amend Register component to use portal

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import { useAuth } from "../hooks/useAuth.jsx";
+import ReactDOM from "react-dom";
 
-function Register() {
+function Register(props) {
 
   const auth = useAuth();
 
@@ -10,10 +11,13 @@ function Register() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  async function requestRegister() {
+  function closeModal() {
+    props.setRegisterOpen(false);
+  }
+
+  function requestRegister() {
     if (isLoading) {
       auth.register(name, email, password);
-      setIsLoading(false);
       setName('');
       setEmail('');
       setPassword('');
@@ -30,78 +34,93 @@ function Register() {
     setIsLoading(true);
   }
 
+  function handleRegister() {
+    if (auth.user) {
+      closeModal();
+    };
+  }
+
   useEffect(() => {
     requestRegister();
   }, [isLoading]);
 
-  return (
-    <div>
-      <input type="checkbox" id="modal-register" className="modal-toggle" hidden />
-      <label htmlFor="modal-register" className="modal cursor-pointer">
-        <label className="modal-box relative" htmlFor="">
-          <h3 className="flex justify-center font-bold text-lg">register</h3>
-          <form 
-            action="#"
-            className="grid items-center justify-center"
-            onSubmit={requestRegister}
-          >
-            <input 
-              type="text" 
-              autoComplete="name"
-              onKeyDown={event => {
-                if (event.key === 'Enter') {
-                  handleInput(event);
-                } else if (event.key === 'Escape') {
-                  return;   // TODO add cancel function
-                }
-              }}
-              placeholder="name" 
-              className="mt-4 input input-bordered w-full max-w-xs" 
-            />
-            <input 
-              type="text" 
-              autoComplete="username"
-              onKeyDown={event => {
-                if (event.key === 'Enter') {
-                  handleInput(event);
-                } else if (event.key === 'Escape') {
-                  return;   // TODO add cancel function
-                }
-              }}
-              placeholder="email" 
-              className="mt-4 input input-bordered w-full max-w-xs" 
-            />
-            <input 
-              type="password" 
-              autoComplete="current-password"
-              onKeyDown={event => {
-                if (event.key === 'Enter') {
-                  handleInput(event);
-                } else if (event.key === 'Escape') {
-                  return;   // TODO add cancel function
-                }
-              }}
-              placeholder="password" 
-              className="mt-4 input input-bordered w-full max-w-xs" 
-            />
-            { auth.error && (
-              <div className="grid place-items-center text-xs mt-2">
-                <p>Registration error</p>
-              </div>
-            )}
-            <button 
-              className="grid justify-center btn btn-primary mt-3"
-              onClick={event => {
-                handleInput(event)
-              }}
+  useEffect(() => {
+    handleRegister();
+  }, [auth.user])
+
+  return ReactDOM.createPortal(
+    <>
+      <div 
+        className="fixed h-full w-full bg-gray-800 opacity-70 top-0"
+        onClick={closeModal}
+      />
+        <div className="grid place-items-center">
+          <div className="fixed modal-box w-2xl max-w-2xl top-20">
+            <h3 className="flex justify-center font-bold text-lg">register</h3>
+            <form 
+              action="#"
+              className="grid items-center justify-center"
+              onSubmit={requestRegister}
             >
-              submit
-            </button>
-          </form>
-        </label>
-      </label>
-    </div>
-  )
+              <input 
+                type="text" 
+                onKeyDown={event => {
+                  if (event.key === 'Enter') {
+                    handleInput(event);
+                  } else if (event.key === 'Escape') {
+                    closeModal();
+                  }
+                }}
+                placeholder="name" 
+                className="mt-4 input input-bordered w-full max-w-xs" 
+              />
+              <input 
+                type="text" 
+                onKeyDown={event => {
+                  if (event.key === 'Enter') {
+                    handleInput(event);
+                  } else if (event.key === 'Escape') {
+                    closeModal();
+                  }
+                }}
+                placeholder="email" 
+                className="mt-4 input input-bordered w-full max-w-xs" 
+              />
+              <input 
+                type="password" 
+                autoComplete="current-password"
+                onKeyDown={event => {
+                  if (event.key === 'Enter') {
+                    handleInput(event);
+                  } else if (event.key === 'Escape') {
+                    closeModal();
+                  }
+                }}
+                placeholder="password" 
+                className="mt-4 input input-bordered w-full max-w-xs" 
+              />
+              { auth.error && (
+                <div className="grid place-items-center text-xs mt-2">
+                  <p>Registration error</p>
+                </div>
+              )}
+              <button 
+                className="grid justify-center btn btn-primary mt-3"
+                onClick={event => {
+                  handleInput(event)
+                }}
+              >
+                submit
+              </button>
+              { isLoading && (
+                <progress className="progress flex mt-4"></progress>
+              )}
+            </form>
+          </div>
+        </div>
+    </>,
+    document.getElementById("register-modal"),
+  );
 }
 
 export default Register
