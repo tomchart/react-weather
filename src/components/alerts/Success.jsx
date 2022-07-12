46ed047ab843ffee825e9a5a56fcbd6dc7399bcb
Tom Hart <tommy-hart@hotmail.com>
2022-07-12 12:38:12 +0100

    add alert components for flash messages

---
+++ src/components/alerts/Success.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from "react-dom";
+
+export default function Success(props) {
+  return ReactDOM.createPortal(
+    <div className="fixed bottom-3 right-3">
+      <div class="alert alert-success shadow-lg max-w-xs">
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+          <span>{props.message}</span>
+        </div>
+      </div>
+    </div>,
+    document.getElementById("alert"),
+  );
+}
+
