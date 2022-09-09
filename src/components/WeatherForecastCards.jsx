9aa9655ab772dda5c10a1b4a599eb52627b13dbb
Tom Hart <tommy-hart@hotmail.com>
2022-09-09 17:02:43 +0100

    fix WeatherForecastCards

--- src/components/WeatherForecastCards.jsx
+++ src/components/WeatherForecastCards.jsx
@@ -16,11 +16,11 @@ function WeatherForecastCards () {
     <div className="inline-flex">
       { futureDays.map((dayObject, index) =>
         <div key={index} className="p-2 bg bg-slate-700 rounded-xl ml-2 mr-2 grid place-items-center">
-          <p>{weekday[new Date(dayObject.datetime).getDay()]}</p>
+          <p>{weekday[new Date(dayObject.date).getDay()]}</p>
           <div className="w-16 grid place-items-center">
             <div className="inline-flex">
-              <p className="font-semibold mr-1">{dayObject.tempmax}</p>
-              <p className="font-light">{dayObject.tempmin}</p>
+              <p className="font-semibold mr-1">{dayObject.tempMax}</p>
+              <p className="font-light">{dayObject.tempMin}</p>
             </div>
           </div>
         </div>
