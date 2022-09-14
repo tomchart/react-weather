70df6594dd2c76fd21b0db2e945ed457804acb66
Tom Hart <tommy-hart@hotmail.com>
2022-09-14 11:42:07 +0100

    add icons to WeatherForecastCards

--- src/components/WeatherForecastCards.jsx
+++ src/components/WeatherForecastCards.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { WeatherContext } from "../context/WeatherContext.js";
+import sprite from "../components/icons/sprite.svg";
 
 function WeatherForecastCards () {
 
@@ -17,6 +18,9 @@ function WeatherForecastCards () {
       { futureDays.map((dayObject, index) =>
         <div key={index} className="p-2 bg bg-slate-700 rounded-xl ml-2 mr-2 grid place-items-center">
           <p>{weekday[new Date(dayObject.date).getDay()]}</p>
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" stroke="#a6adba" fill="none">
+            <use xlinkHref={`${sprite}#${dayObject.iconName}`} />
+          </svg>
           <div className="w-16 grid place-items-center">
             <div className="inline-flex">
               <p className="font-semibold mr-1">{dayObject.tempMax}</p>
